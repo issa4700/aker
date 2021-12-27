9a604f45dccb6ec68055ebb4aed8cf76ae723a6c
issa4700 <issa4700@gmail.com>
2021-12-27 00:58:11 +0000

    Removed redundant stuff

--- components/whitelist/Players.jsx
+++ components/whitelist/Players.jsx
@@ -39,7 +39,7 @@ export default function Players({ props }) {
 
   return (
     <>
-      {players.reverse().map((player) => (
+      {players.map((player) => (
         <tr key={player}>
           <td className="whitespace-nowrap">
             <div className="px-4 py-3 flex items-center">
