a8fd4d65fd60e8ac3c394c9726a618022a8dca46
issa4700 <issa4700@gmail.com>
2022-01-28 21:17:03 +0000

    Changed from MongoDB to SQL

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,25 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb.js";
+import SequelizeAdapter, { models } from "@next-auth/sequelize-adapter";
+import sequelize from "../../../lib/sqlize";
+import Application from "../../../lib/models/application";
+import { DataTypes } from "sequelize";
+
+sequelize.sync();
 
 export default NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
-
   secret: `${process.env.JWT_SECRET}`,
+
+  adapter: SequelizeAdapter(sequelize, {
+    models: {
+      User: sequelize.define("user", {
+        ...models.User,
+        minecraftUUID: DataTypes.UUID,
+      }),
+
+      Application: Application,
+    },
+  }),
 
   // Configure one or more authentication providers
   providers: [
